aca6f40915fe15ef8e59c564e1195f3d02df622a
sztxr <sztxr@github.com>
2019-10-09 15:32:47 -0300

    part2-4_v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -8,10 +8,10 @@ const App = (props) => {
   const [showAll, setShowAll] = useState(true)
 
   const hook = () => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }
   useEffect(hook, [])
@@ -19,27 +19,6 @@ const App = (props) => {
   const notesToShow = showAll
     ? notes
     : notes.filter(note => note.important) // note.important === true
-
-  const toggleImportance = id => {
-    // console.log(`importance of ${id} needs to be toggled`)
-
-    // define unique url for each note resource based on its id
-    const url = `http://localhost:3001/notes/${id}`
-    // array find method to find the note we want to modify, and assing it to the `note` variable
-    const note = notes.find(n => n.id === id)
-    // then we create a new object that is the exact copy of the note we saved,
-    // apart from the important property, which we toggle to be the opposite of its previous value
-    const changedNote = { ...note, important: !note.important } // shallow copy
-
-    // The callback function sets the component's notes state to a new array that
-    // contains all the items from the previous notes array, except for the old note
-    // which is replaced by the updated version of it returned by the server:
-    axios
-      .put(url, changedNote)
-      .then(res => setNotes(notes.map(note => note.id !== id ? note : res.data)))
-    // if note.id !== id is true, we simply copy the item from the old array into the new array
-    // if the condition is false, then the note object returned by the server is added to the array instead.
-  }
 
   const rows = () => notesToShow.map(note =>
     <Note
@@ -49,6 +28,11 @@ const App = (props) => {
     />
   )
 
+  const handleNoteChange = (e) => {
+    // console.log(e.target.value)
+    setNewNote(e.target.value)
+  }
+
   const addNote = (e) => {
     e.preventDefault()
     const noteObject = {
@@ -56,22 +40,43 @@ const App = (props) => {
       date: new Date().toISOString(),
       important: Math.random() > 0.5
     }
-    // // setNotes(notes.concat(noteObject))
-    // setNotes([...notes, noteObject])
-    // setNewNote('')
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(res => {
-        // console.log(res)
-        setNotes([...notes, res.data])
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes([...notes, returnedNote])
         setNewNote('')
       })
   }
 
-  const handleNoteChange = (e) => {
-    // console.log(e.target.value)
-    setNewNote(e.target.value)
+  const toggleImportance = id => {
+    // console.log(`importance of ${id} needs to be toggled`)
+
+    // array find method to find the note we want to modify, and assing it to the `note` variable
+    const note = notes.find(n => n.id === id)
+    // then we create a new object that is the exact copy of the note we saved,
+    // apart from the important property, which we toggle to be the opposite of its previous value
+    const changedNote = { ...note, important: !note.important } // shallow copy
+
+    // The callback function sets the component's notes state to a new array that
+    // contains all the items from the previous notes array, except for the old note
+    // which is replaced by the updated version of it returned by the server:
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        // if note.id !== id is true, we simply copy the item from the old array into the new array
+        // if the condition is false, then the note object returned by the server is added to the array instead.
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(err => {
+        alert(`the note '${note.content}' was already deleted from server`)
+        //return an array with only the items from the list for which n.id !== id return true for
+        setNotes(notes.filter(n => n.id !== id))
+      })
+
   }
+
+
 
   return (
     <div>
