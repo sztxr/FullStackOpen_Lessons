cc0b621c2e52813e675c23ba830cac8f3fc28d9f
sztxr <sztxr@github.com>
2019-11-05 22:18:41 -0300

    part4: 2/b supertest

---
+++ backend/test/note_api.test.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/notes')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are three notes', async () => {
+  const response = await api.get('/api/notes')
+  expect(response.body.length).toBe(3)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+  expect(response.body[0].content).toBe('HTML is Easy!')
+})
+
+afterAll((done) => {
+  done()
+  mongoose.connection.close()
+})
