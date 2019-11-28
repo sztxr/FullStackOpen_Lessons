17c2bd49f6ecd8449f4b552e4b9cca0e3eccbfda
sztxr <sztxr@github.com>
2019-11-27 23:13:51 -0300

    part4: 3/c | mongoose schema for users

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,13 @@
 // The user.js file under the models directory only defines the Mongoose schema for users.
 
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const noteSchema = new mongoose.Schema({
-  username: String,
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -13,8 +17,9 @@ const noteSchema = new mongoose.Schema({
     }
   ],
 })
+userSchema.plugin(uniqueValidator)
 
-noteSchema.set('toJSON', {
+userSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
@@ -24,4 +29,4 @@ noteSchema.set('toJSON', {
   }
 })
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('User', userSchema)
