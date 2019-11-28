c72061b8ad1d74770efe11e5359cc8ec2c616887
sztxr <sztxr@github.com>
2019-11-28 00:32:14 -0300

    part4: 4/d | add authentication to limit creating new notes to logged in users

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -19,6 +19,15 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 // GET
 
@@ -59,16 +68,27 @@ notesRouter.get('/:id', async (request, response, next) => {
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
-  const user = await User.findById(body.userId)
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-    user: user._id
-  })
+  const token = getTokenFrom(request)
 
   try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      // 401 unauthorized
+      return response.status(401).json({
+        error: 'token missing or invalid'
+      })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+      user: user._id
+    })
+
     const savedNote = await note.save()
     user.notes = user.notes.concat(savedNote._id)
     await user.save()
