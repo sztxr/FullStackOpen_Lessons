fa6a06c388b2cf5ec4c1271002d5d5faca97e326
sztxr <sztxr@github.com>
2019-12-12 03:08:40 -0300

    part5: 1/a | login in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import noteService from './services/notes'
 import Note from './components/Note'
 import Notification from './components/Notification'
 import Footer from './components/Footer'
+import loginService from './services/login'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   const hook = () => {
     noteService
@@ -23,12 +27,73 @@ const App = (props) => {
     ? notes
     : notes.filter(note => note.important) // note.important === true
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    // console.log('logging in with', username, password)
+
+    // If the login is successful, the form fields are emptied and the server response
+    // (including a token and the user details) is saved to the user field of the application's state.
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    // If the login fails, or running the function loginService.login results in an error, the user is notified.
+    catch (exception) {
+      setErrorMessage('Invalid credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
   const rows = () => notesToShow.map(note =>
     <Note
       key={note.id}
       note={note}
       toggleImportance={() => toggleImportanceOf(note.id)}
     />
+  )
+
+  const loginForm = () => (
+    // An object is given to the event handler as a parameter,
+    // and it destructures the field target from the object and save its value to the state.
+    <form onSubmit={handleLogin} className="form-login">
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="text"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        type="text"
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button className="btn btn-primary" type="submit">Add</button>
+    </form>
   )
 
   const handleNoteChange = (e) => {
@@ -86,6 +151,20 @@ const App = (props) => {
 
       <Notification message={errorMessage} />
 
+      <h2>Login</h2>
+      
+      {/* {user === null && loginForm()} */}
+      {/* {user !== null && noteForm()} */}
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      }
+
+      <br />
+
       <div>
         <button className='btn btn-primary' onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -95,15 +174,7 @@ const App = (props) => {
       <ul className='note-container'>
         {rows()}
       </ul>
-
-      <form onSubmit={addNote}>
-        <input
-          value={newNote}
-          onChange={handleNoteChange}
-        />
-        <button className='btn btn-primary' type='submit'>Add</button>
-      </form>
-
+      
       <Footer />
     </div>
   )
