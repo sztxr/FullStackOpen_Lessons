ea7be51245dcb05f568cce3cf9c3a9a1cbaeec3f
sztxr <sztxr@github.com>
2019-12-17 17:51:40 -0300

    part5: 2/b | add PropTypes

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({
   handleSubmit,
@@ -28,6 +29,14 @@ const LoginForm = ({
       <button type="submit" className="btn btn-primary">Login</button>
     </form>
   )
+}
+
+LoginForm.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm
