3baec8b5a2d5736b3c3da439dbd7ea5a41dde85d
sztxr <sztxr@github.com>
2019-12-28 03:59:39 -0300

    part5: 3/c | clicking buttons in tests

--- frontend/src/tests/Note.test.js
+++ frontend/src/tests/Note.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Note from '../components/Note'
 
@@ -33,4 +33,22 @@ test('renders content', () => {
   expect(div).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
+})
+
+test('clicking the button calls event handler once', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = jest.fn()
+
+  const { getByText } = render(
+    <Note note={note} toggleImportance={mockHandler} />
+  )
+
+  const button = getByText('mark as not important')
+  fireEvent.click(button)
+
+  expect(mockHandler.mock.calls.length).toBe(1)
 })
