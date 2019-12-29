af37dde9d44520a996492ad8ce26d5f343ea25d4
sztxr <sztxr@github.com>
2019-12-29 16:17:42 -0300

    part5: 4/d | custom hooks

---
+++ frontend/src/hooks/useField.js
@@ -0,0 +1,11 @@
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = e => { setValue(e.target.value) }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
