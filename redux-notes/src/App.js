83d356546674ffc2e0c809f3b3e88f4aafd54c48
sztxr <sztxr@github.com>
2019-12-30 19:23:49 -0300

    part6: 2/b | implement filtering for notes (radio buttons only)

--- redux-notes/src/App.js
+++ redux-notes/src/App.js
@@ -3,9 +3,19 @@ import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 
 const App = props => {
+  const filterSelected = value => () => { console.log(value) }
+
   return (
     <div>
       <NewNote store={props.store}/>
+
+      <div>
+        &ndash; all <input type="radio" name="filter" onChange={filterSelected('ALL')}/>
+        &ndash; important <input type="radio" name="filter" onChange={filterSelected('IMPORTANT')}/>
+        &ndash; not important <input type="radio" name="filter" onChange={filterSelected('NOT_IMPORTANT')}/>
+        &ndash;
+      </div>
+
       <Notes store={props.store}/>
     </div>
   )
--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -19,9 +19,9 @@ const noteReducer = (state = initialState, action) => {
     case 'TOGGLE_IMPORTANCE':
       const id = action.data.id
       const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
       }
       return state.map(note => note.id !== id ? note : changedNote)
     default:
