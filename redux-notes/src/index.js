e553bd1142efbc8487509e4b58745889b914a2ef
sztxr <sztxr@github.com>
2019-12-30 15:15:03 -0300

    part6: 1/a | uncontrolled form

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -24,12 +24,41 @@ store.dispatch({
   }
 })
 
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+
 function App() {
+  const addNote = e => {
+    e.preventDefault()
+    // because the field has a name, we can access the content via the event object event.target.note.value
+    const content = e.target.note.value
+    store.dispatch({
+      type: 'NEW_NOTE',
+      data: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+    e.target.note.value = ''
+  }
+
+  const toggleImportance = id => () => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      data: { id }
+    })
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note"/>
+        <button type="submit">add</button>
+      </form>
+
       <ul>
         {store.getState().map(note =>
-          <li key={note.id}>
+          <li key={note.id} onClick={toggleImportance(note.id)}>
             {note.content} <strong>{note.important ? '— important' : ''}</strong>
           </li>
         )}
