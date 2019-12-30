b89edaba302570257115b116e974802bee830604
sztxr <sztxr@github.com>
2019-12-30 19:32:11 -0300

    part6: 2/b | combine reducers

--- redux-notes/src/index.js
+++ redux-notes/src/index.js
@@ -1,15 +1,36 @@
 import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import App from './App'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+import { createNote } from './reducers/noteReducer'
+import { filterChange } from './reducers/filterReducer'
 
-const renderApp = () => {
-  ReactDOM.render(<App store={store} />, document.getElementById('root'))
-}
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
 
-renderApp()
-store.subscribe(renderApp)
+const store = createStore(reducer)
+
+// console.log(store.getState())
+
+// store.subscribe(() => console.log(store.getState()))
+store.dispatch(filterChange('IMPORTANT'))
+store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+
+
+// const renderApp = () => {
+//   ReactDOM.render(<App store={store} />, document.getElementById('root'))
+// }
+
+ReactDOM.render(
+  <div></div>,
+  document.getElementById('root')
+)
+
+// renderApp()
+// store.subscribe(renderApp)
--- redux-notes/src/reducers/noteReducer.js
+++ redux-notes/src/reducers/noteReducer.js
@@ -12,6 +12,7 @@ const initialState = [
 ]
 
 const noteReducer = (state = initialState, action) => {
+  console.log('ACTION: ', action)
   switch (action.type) {
     case 'NEW_NOTE':
       // return state.concat(action.data)
