95ac8433b1b8358ddbabb5574c55d087786b12ad
sztxr <sztxr@github.com>
2020-01-07 20:42:48 -0300

    part 8.2 b | Mutation component

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Query, ApolloConsumer } from 'react-apollo'
+import { Query, ApolloConsumer, Mutation } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
 
 const ALL_PERSONS = gql`
 {
@@ -13,15 +14,41 @@ const ALL_PERSONS = gql`
 }
 `
 
+const CREATE_PERSON = gql`
+  mutation createPerson($name: String!, $street: String!, $city: String!, $phone: String) {
+    addPerson(
+      name: $name,
+      street: $street,
+      city: $city,
+      phone: $phone
+    ) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
 const App = () => {
   return (
-    <ApolloConsumer>
-      {(client =>
-        <Query query={ALL_PERSONS}>
-          {(result) => <Persons result={result} client={client} />}
-        </Query>
-      )}
-    </ApolloConsumer>
+    <div>
+      <ApolloConsumer>
+        {(client) =>
+          <Query query={ALL_PERSONS}>
+            {(result) => <Persons result={result} client={client} />}
+          </Query>
+        }
+      </ApolloConsumer>
+
+      <h2>create new</h2>
+      <Mutation mutation={CREATE_PERSON}>
+        {(addPerson) => <PersonForm addPerson={addPerson} />}
+      </Mutation>
+    </div>
   )
 }
 
