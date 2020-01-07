e197396a6ee042659bf41958da950a28d5bef3bd
sztxr <sztxr@github.com>
2020-01-07 05:39:44 -0300

    part 8.1 a | Changing a phone number

--- part8/graphql-phonebook-backend/index.js
+++ part8/graphql-phonebook-backend/index.js
@@ -55,6 +55,10 @@ const typeDefs = gql`
       street: String!
       city: String!
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }  
 `
 
@@ -88,6 +92,14 @@ const resolvers = {
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find(p => p.name === args.name)
+      if (!person) return null
+
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map(p => p.name === args.name ? updatedPerson : p)
+      return updatedPerson
     }
   }
 }
