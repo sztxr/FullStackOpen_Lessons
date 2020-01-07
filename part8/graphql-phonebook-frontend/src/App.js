8b7e17ba0485f3b1618485453c4465618c79234c
sztxr <sztxr@github.com>
2020-01-07 17:34:19 -0300

    part 8.2 b | Apollo client + setup

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,4 +1,26 @@
 import React from 'react'
+import ApolloClient, { gql } from 'apollo-boost'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const query = gql`
+{
+  allPersons  {
+    name,
+    phone,
+    address {
+      street,
+      city
+    }
+    id
+  }
+}
+`
+
+client.query({ query })
+  .then((response) => console.log(response.data))
 
 function App() {
   return (
